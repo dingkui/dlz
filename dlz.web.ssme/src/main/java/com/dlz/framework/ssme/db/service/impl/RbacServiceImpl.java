b27e46737f9cb33eaabbf0f4d7a70428aab5ba87
ps <295054707@qq.com>
2019-09-02 14:45:18 +0800

    将nvl改为ifnull

--- dlz.web.ssme/src/main/java/com/dlz/framework/ssme/db/service/impl/RbacServiceImpl.java
+++ dlz.web.ssme/src/main/java/com/dlz/framework/ssme/db/service/impl/RbacServiceImpl.java
@@ -2,6 +2,7 @@ package com.dlz.framework.ssme.db.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,14 +29,18 @@ public class RbacServiceImpl implements RbacService {
 	}
 
 	public String getCode(String pCode,String codeName,String tableName) throws Exception{
-		SearchParaMap spm = new SearchParaMap(tableName,"nvl(max("+codeName+"),'') as code");
+		SearchParaMap spm = new SearchParaMap(tableName,"ifnull(max("+codeName+"),'') as code");
 		spm.addCondition(codeName, "like", new String[]{"",pCode,"__"});
 //		spm.createPage().setOrderBy("code desc");
 		String o = commService.getStr(spm);
 		if(o==null || "".equals(o)){
 			return pCode+"01";
 		}else{
-			int maxSub=Integer.parseInt(o.substring(pCode.length()),16)+1;
+			String subCodeStr = o.substring(pCode.length());
+			if(StringUtils.isBlank(subCodeStr)){
+				return pCode+"01";
+			}
+			int maxSub=Integer.parseInt(subCodeStr,16)+1;
 			String value=Integer.toHexString(maxSub);
 			if(value.length()==1){
 				return pCode+"0"+value.toUpperCase();
